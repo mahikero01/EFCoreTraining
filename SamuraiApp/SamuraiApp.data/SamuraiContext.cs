5102044d9ff796d8627188027dff81b92c785e94
Sarmiento <sarmife@prd.manulifeusa.com>
2018-01-02 14:48:44 -0500

    3 -
    Creating the Data Model

---
+++ SamuraiApp/SamuraiApp.data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
