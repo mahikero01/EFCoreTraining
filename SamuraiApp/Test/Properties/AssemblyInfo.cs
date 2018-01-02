f1ced79ac1e9cc4d6aced7ce634081ae4ddad636
Sarmiento <sarmife@prd.manulifeusa.com>
2018-01-02 12:19:26 -0500

    Initial commit

---
+++ SamuraiApp/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MNA")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © MNA 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bb7d9ea-7a29-418e-99a5-ac549823f37e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
